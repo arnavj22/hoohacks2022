11af9c52599d6745876cb4584c5422a36ed674bf
Sameer Gabbita <samgabbita@gmail.com>
2022-03-26 16:01:56 -0400

    added features page

--- website/run.py
+++ website/run.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+def model():
+    return render_template('analyze.html')
+
 if __name__ == '__main__':
-     app.run(host = '127.0.0.1', port = 5000)
+     app.run(debug=True)
